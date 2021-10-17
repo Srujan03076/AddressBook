eceb737d18fec4daad9756c9e8f33865c45f5bc6
“Srujan03076” <srujanmahaja5@gmail.com>
2021-10-17 10:05:46 +0530

    Adding Welcome Message

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿//using System;
 
-namespace AdressBook
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welocme to Address Book");
-            Console.ReadLine();
-        }
-    }
-}
+//namespace AdressBook
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Welocme to Address Book");
+//            Console.ReadLine();
+//        }
+//    }
+//}
